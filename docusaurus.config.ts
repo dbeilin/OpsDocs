a437c3bcfe87059173711fc4db58d6937ba2697f
Daniel Beilin <beilin@savvy.security>
2024-12-15 15:45:54 +0200

    baseurl update

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   url: 'https://dbeilin.github.io',
-  baseUrl: '/',
+  baseUrl: '/OpsDocs/',
 
   organizationName: 'dbeilin',
   projectName: 'OpsDocs',
